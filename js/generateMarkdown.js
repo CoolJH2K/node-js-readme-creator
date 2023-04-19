4fb91e4e887f42e77948b6b1fb139b6f7c40908b
Janna Harmon <harmonjanna00@gmail.com>
2023-04-18 22:01:20 -0400

    Added some blank 'if/else' statements in generateMarkdown.js.

--- js/generateMarkdown.js
+++ js/generateMarkdown.js
@@ -2,17 +2,34 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   // This will use data passed in from the generateMarkdown funciton. Have a logic gate to make sure a license is chosen
-  return `https://img.shields.io/badge/License-{}`
+  if () {
+
+  } else {
+    return "";
+  }
+  return `https://img.shields.io/badge/License-${license}-blue`
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+  if () {
+
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   // This will use data passed in from the generateMarkdown function. Have a logic gate to make sure a license is chosen.
+  if () {
+    
+  } else {
+    
+  }
 }
 
 // TODO: Create a function to generate markdown for README
